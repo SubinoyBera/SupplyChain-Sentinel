48a57c2b05dfabb758375e973c038033f97d6274
Subinoy Bera <subinoyberadgp@gmail.com>
2024-11-28 18:55:07 +0530

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import find_packages,setup
+import setuptools
+from setuptools import find_packages, setup
 from typing import List
 
-CONNECTOR="-e ."
+CONNECTOR= "-e ."
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
@@ -13,15 +14,25 @@ def get_requirements(file_path:str)->List[str]:
     
     return requirements
 
+with open("README.md", 'r', encoding="utf-8") as f:
+    long_description= f.read()
 
 setup(
     name="SupplyChain_MLWorkflow Project",
     version="0.0.01",
     author="Subinoy Bera",
     author_email="subinoyberadgp@gmail.com",
-    packages=find_packages(),
+    description="A small end to end python package for machine learning app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url="https://github.com/SubinoyBera/SupplyChain_MLWflow",
+    project_urls={
+        "Bug Tracker": "https://github.com/SubinoyBera/SupplyChain_MLWflow/issues"
+    },
     install_requires=get_requirements("requirements.txt"),
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
     extras_require={'dev':['jupyter']},
-    python_requires='>=3.11.0',
+    python_requires=">=3.11.0",
     license="Apache 2.0"
 )
