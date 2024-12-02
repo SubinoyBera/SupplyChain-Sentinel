c0e600e05c92bce7614972dc95a36eb123859cc1
Subinoy Bera <subinoyberadgp@gmail.com>
2024-12-02 13:40:25 +0530

    add data transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+class DataTransformationConfig:
+    def __init__():
+        pass
+    
+    def get_data_path() -> Path:
+        pass
+    
+class DataTransformation:
+    def __init__():
+        pass
