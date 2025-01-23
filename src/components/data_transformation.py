1831e07288b31b632a452a0670f25cb72f6040b0
Subinoy Bera <144976707+SubinoyBera@users.noreply.github.com>
2025-01-24 00:19:21 +0530

    add data transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -8,12 +8,7 @@ from src.logger import logging
 from utils import read_yaml, create_directories
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from imblearn.over_sampling import SMOTE, ADASYN
-from imblearn.under_sampling import EditedNearestNeighbours
-from imblearn.combine import SMOTETomek
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 class DataTransformationConfig:
     def __init__(self):
@@ -26,43 +21,37 @@ class DataTransformationConfig:
             create_directories([config.root_dir])
             root_dir= config.root_dir
             data_path= config.file_name
-            data_transformation_config= Path(root_dir, data_path)
-            logging.info("Created data_ingestion path")
+            logging.info("Created data_transformation path")
             
-            return data_transformation_config
+            return (root_dir, 
+                    data_path
+                   )
         
         except Exception as e:
             logging.error(e, exc_info=True)
             raise CustomException(e,sys)
 
-    
+
 class DataTransformation:
-    def __init__(self, config=DataTransformationConfig):
-        self.config = config
+    def __init__(self, config:DataTransformationConfig):
+        self.root_dir, self.data_path= config.get_data_transformation_config()
        
     def LabelEncoding(self, x):
         le=LabelEncoder()
         x=le.fit_transform(x)
         return x
     
-    
-    def get_data_balancing(self, train):
-        pass
-    
     def get_transformation(self):
-        
-        data=pd.read_csv(self.config.data_path)
+        data= pd.read_csv(self.config.data_path)
         
         data['Suspected_Fraud']= np.where(['Order_Status']=='SUSPECTED_FRAUD', 1, 0)
         
         df= data['Type', 'Customer_Id', 'Order_Customer_Id', 'Order_Region', 'Customer_Country', 
-                        'Customer_City', 'Customer_Segment', 'Order_City', 'Order_State', 
-                        'Order_Country', 'Late_DeliveryRisk', 'Shipping_Mode', 'Suspected_Fraud']
+                'Customer_City', 'Customer_Segment', 'Order_City', 'Order_State', 
+                'Order_Country', 'Late_DeliveryRisk', 'Shipping_Mode', 'Suspected_Fraud']
             
         df= df.apply(self.LabelEncoding())
-        
         train, test= train_test_split(df, test_size=0.2, random_state=42)
-        
-        test.to_csv(os.path.join(self.config.root_dir), index=False)
-        
-        self.get_data_balancing(train)
+
+        train.to_csv(os.path.join(self.config.root_dir/'train.csv'), index=False)
+        test.to_csv(os.path.join(self.config.root_dir/'test.csv'), index=False)
