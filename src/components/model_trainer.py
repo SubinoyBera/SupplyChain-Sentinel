e5f2fbe7f0fd86ded95945416cd78e4870d8ccd3
Subinoy Bera <144976707+SubinoyBera@users.noreply.github.com>
2025-01-30 23:52:40 +0530

    update model trainer

---
+++ src/components/model_trainer.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from pathlib import Path
+from src.exception import CustomException
+from src.logger import logging
+from utils import read_yaml, create_directories
+
+import mlflow
+import dagshub
+
+class ModelTrainerConfig:
+  def __init__():
+    pass
+
+class ModelTrainer:
+  def __init__():
+    pass
+    
+  def initiate_model_training(self, X_train, y_train):
+    try:
+      pass
+
+    except Exception as e:
+      pass
