27c667cd0d2e239b193d6b8e62ec788e0a86c326
Subinoy Bera <subinoyberadgp@gmail.com>
2025-02-23 23:31:57 +0530

    add experimentation tracking

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,9 @@ import box
 from box import ConfigBox
 from ensure import ensure_annotations
 from pathlib import Path
+import sklearn
+from sklearn.metrics import classification_report
+import mlflow
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
@@ -26,5 +29,27 @@ def create_directories(path_to_directories: list, verbose=True):
         os.makedirs(path, exist_ok=True)
         if verbose:
             logging.info(f"Created directory: {path}")
+            
+            
+def evaluate_model(model, X_test, y_test):
+    try: 
+        y_pred= model.predict(X_test)
+        report= classification_report(y_test, y_pred, output_dict=True)
+        return report
 
+    except Exception as e:
+        logging.error(f"Error in {model} model evaluation: {e,sys}")
+        
 
+def track_experiment(model_name, model, model_params, report):
+    with mlflow.start_run(run_name=model_name):
+        mlflow.log_param('model_name', model_params)
+        mlflow.log_metric('accuracy', report['accuracy'])
+        mlflow.log_metric('recall_class_1', report['1']['recall'])
+        mlflow.log_metric('recall_class_0', report['0']['recall'])
+        mlflow.log_metric('f1-score_macro', report['macro avg']['f1-score'])
+        
+        if "XGB" in model_name:
+            mlflow.xgboost.log_model(model, "model")
+        else:
+            mlflow.sklearn.log_model(model, "model")
